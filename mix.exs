18f0c0d3c3080941592a892816793ae11ae4340d
slothopher <aetherus.zhou@gmail.com>
2020-08-19 11:12:15 +0800

    Import terms

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Termina.MixProject do
   def project do
     [
       app: :termina,
-      version: "0.1.2",
+      version: "0.2.0",
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
